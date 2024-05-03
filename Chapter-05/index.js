2e891030c02c8e8105ebfad177d41a0abd67719f
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-03 06:55:26 +0500

    Update index.js

--- Chapter-05/index.js
+++ Chapter-05/index.js
@@ -1,152 +1,153 @@
-// QUESTION 01, 02
 
-// var num1 = 3
-// var num2 = 5
-// document.write(`sum of 3 and 5 is ${num1+num2} <br/>`)
+// QUESTION 01, 02) ******************************************************************************************************************
 
-// var num1 = 3
-// var num2 = 5
-// document.write(`sum of 3 and 5 is ${num1-num2} <br/>`)
+var num1 = 3
+var num2 = 5
+document.write(`sum of 3 and 5 is ${num1+num2} <br/>`)
 
-// var num1 = 3
-// var num2 = 5
-// document.write(`sum of 3 and 5 is ${num1*num2} <br/>`)
+var num1 = 3
+var num2 = 5
+document.write(`sum of 3 and 5 is ${num1-num2} <br/>`)
 
-// var num1 = 3
-// var num2 = 5
-// document.write(`sum of 3 and 5 is ${num1/num2} <br/>`)
+var num1 = 3
+var num2 = 5
+document.write(`sum of 3 and 5 is ${num1*num2} <br/>`)
 
-// var num1 = 3
-// var num2 = 5
-// document.write(`sum of 3 and 5 is ${num1%num2} <br/>`)
+var num1 = 3
+var num2 = 5
+document.write(`sum of 3 and 5 is ${num1/num2} <br/>`)
 
-// // QUESTION 03
+var num1 = 3
+var num2 = 5
+document.write(`sum of 3 and 5 is ${num1%num2} <br/>`)
 
-// var declare;
-// document.write("Value after variable declaration is ",declare, "<br/>" );
-// declare = 5;
-// document.write("initial value: ", declare , "<br/>");
-// declare = ++declare;
-// document.write("value after increment is : " , declare , "<br/>");
-// declare = declare + 7;
-// document.write("value after addition is : ", declare, "<br/>");
-// declare = --declare;
-// document.write("value after decrement is : ", declare, "<br/>");
-// declare = declare%3
-// document.write("the remainder is : ", declare, "<br/>");
+// QUESTION 03) ******************************************************************************************************************
 
-// // QUESTION 04
+var declare;
+document.write("Value after variable declaration is ",declare, "<br/>" );
+declare = 5;
+document.write("initial value: ", declare , "<br/>");
+declare = ++declare;
+document.write("value after increment is : " , declare , "<br/>");
+declare = declare + 7;
+document.write("value after addition is : ", declare, "<br/>");
+declare = --declare;
+document.write("value after decrement is : ", declare, "<br/>");
+declare = declare%3
+document.write("the remainder is : ", declare, "<br/>");
 
-// var ticketPrice = 600;
-// var totalticketPrice = ticketPrice*5;
-// document.write("total cost to 5 tickets to a movie is ", totalticketPrice, "PKR");
+// QUESTION 04) ******************************************************************************************************************
 
-// // QUESTION 05
+var ticketPrice = 600;
+var totalticketPrice = ticketPrice*5;
+document.write("total cost to 5 tickets to a movie is ", totalticketPrice, "PKR");
 
-// var table = +prompt("enter any number ", 4);
-// document.write("table of ", table, "<br/>");
-// document.write(table, " x ", " 1 ", " = ", table , "<br/>");
-// document.write(table, " x ", " 2 ", " = ", table, "<br/>");
-// document.write(table, " x ", " 3 ", " = ", table, "<br/>");
-// document.write(table, " x ", " 4 ", " = ", table ,"<br/>");
-// document.write(table, " x ", " 5 ", " = ", table , "<br/>");
-// document.write(table, " x ", " 6 ", " = ", table, "<br/>");
-// document.write(table, " x ", " 7 ", " = ", table , "<br/>");
-// document.write(table, " x ", " 8 ", " = ", table , "<br/>");
-// document.write(table, " x ", " 9 ", " = ", table, "<br/>");
-// document.write(table, " x ", " 10", " = ", table , "<br/>");
+// QUESTION 05) ******************************************************************************************************************
 
-// // QUESTION 06
+var table = +prompt("enter any number ", 4);
+document.write("table of ", table, "<br/>");
+document.write(table, " x ", " 1 ", " = ", table , "<br/>");
+document.write(table, " x ", " 2 ", " = ", table, "<br/>");
+document.write(table, " x ", " 3 ", " = ", table, "<br/>");
+document.write(table, " x ", " 4 ", " = ", table ,"<br/>");
+document.write(table, " x ", " 5 ", " = ", table , "<br/>");
+document.write(table, " x ", " 6 ", " = ", table, "<br/>");
+document.write(table, " x ", " 7 ", " = ", table , "<br/>");
+document.write(table, " x ", " 8 ", " = ", table , "<br/>");
+document.write(table, " x ", " 9 ", " = ", table, "<br/>");
+document.write(table, " x ", " 10", " = ", table , "<br/>");
 
-// var cel = 76;
-// fah = (cel *9/5)+ 32
-// document.write(cel, "<sup>o</sup>C is ", fah, "<sup>o</sup>F <br/>");
+// QUESTION 06) ******************************************************************************************************************
 
-// var fah = 234;
-// cel = (fah - 32)* 5/9;
-// document.write(cel, "<sup>o</sup>C is ", fah, "<sup>o</sup>F ")
+var cel = 76;
+fah = (cel *9/5)+ 32
+document.write(cel, "<sup>o</sup>C is ", fah, "<sup>o</sup>F <br/>");
 
-// QUESTION 07
+var fah = 234;
+cel = (fah - 32)* 5/9;
+document.write(cel, "<sup>o</sup>C is ", fah, "<sup>o</sup>F ")
 
-// document.write("<h1>Shopping Cart</h1><br/>")
+// QUESTION 07) ******************************************************************************************************************
 
-// var item_1_price = +prompt("Enter price of item 1:");
-// document.write("Price of item 1 is ",item_1_price,"<br/>");
+document.write("<h1>Shopping Cart</h1><br/>")
 
-// var order_quantity_1 = +prompt("How many times item 1 do you want to purchase:");
-// document.write("Quantity of item 1 is ",order_quantity_1,"<br/>");
+var item_1_price = +prompt("Enter price of item 1:");
+document.write("Price of item 1 is ",item_1_price,"<br/>");
 
-// var item_2_price = +prompt("Enter price of item 2:");
-// document.write("Price of item 2 is ",item_2_price,"<br/>");
+var order_quantity_1 = +prompt("How many times item 1 do you want to purchase:");
+document.write("Quantity of item 1 is ",order_quantity_1,"<br/>");
 
-// var order_quantity_2 = +prompt("How many times item 2 do you want to purchase:");
-// document.write("Quantity of item 2 is ",order_quantity_2,"<br/>");
+var item_2_price = +prompt("Enter price of item 2:");
+document.write("Price of item 2 is ",item_2_price,"<br/>");
 
-// var shipping_charges = 100;
-// document.write("Shipping charges ",shipping_charges,"<br/>");
+var order_quantity_2 = +prompt("How many times item 2 do you want to purchase:");
+document.write("Quantity of item 2 is ",order_quantity_2,"<br/>");
 
-// var total_cost = item_1_price*order_quantity_1+item_2_price*order_quantity_2+shipping_charges;
-// document.write("Total cost of your order is: ",total_cost);
+var shipping_charges = 100;
+document.write("Shipping charges ",shipping_charges,"<br/>");
 
-// QUESTION 08
+var total_cost = item_1_price*order_quantity_1+item_2_price*order_quantity_2+shipping_charges;
+document.write("Total cost of your order is: ",total_cost);
 
-// document.write("<h1>Marks Sheet</h1><br/>");
+// QUESTION 08) ******************************************************************************************************************
 
-// var total_marks = +prompt("Enter total marks:");
-// document.write("Total marks: ",total_marks,"<br/>");
+document.write("<h1>Marks Sheet</h1><br/>");
 
-// var obtained_marks = +prompt("Enter obtained marks");
-// document.write("Marks obtained: ",obtained_marks,"<br/>");
+var total_marks = +prompt("Enter total marks:");
+document.write("Total marks: ",total_marks,"<br/>");
 
-// var per = (obtained_marks/total_marks)*100;
-// document.write("Percentage: ",per);
+var obtained_marks = +prompt("Enter obtained marks");
+document.write("Marks obtained: ",obtained_marks,"<br/>");
 
-// QUESTION 09
+var per = (obtained_marks/total_marks)*100;
+document.write("Percentage: ",per);
 
-// document.write("<h1>Currency in PKR</h1><br/>");
+// QUESTION 09) ******************************************************************************************************************
 
-// let usDollar = 300;
-// let dollar = +prompt("How much US Dollars you want?");
+document.write("<h1>Currency in PKR</h1><br/>");
 
-// let saudiRiyals = 75;
-// let riyals = +prompt("How much Saudi Riyals you want?");
+let usDollar = 300;
+let dollar = +prompt("How much US Dollars you want?");
 
-// document.write("Total Currency in PKR: " , usDollar*dollar+saudiRiyals*riyals);
+let saudiRiyals = 75;
+let riyals = +prompt("How much Saudi Riyals you want?");
 
-// QUESTION 10
+document.write("Total Currency in PKR: " , usDollar*dollar+saudiRiyals*riyals);
 
-// var in_value = +prompt("Enter any number");
-// document.write((in_value+5*10)/2);
+// QUESTION 10) ******************************************************************************************************************
 
-// QUESTION 11
+var in_value = +prompt("Enter any number");
+document.write((in_value+5*10)/2);
 
-// document.write("<h1>Age Calculator</h1><br/>");
+// QUESTION 11) ******************************************************************************************************************
 
-// var currentYear = +prompt("Enter Current Year");
-// document.write("Current Year: ",currentYear,"<br>");
+document.write("<h1>Age Calculator</h1><br/>");
 
-// var birthYear = +prompt("Enter Birth Year");
-// document.write("Birth Year: ",birthYear,"<br>");
+var currentYear = +prompt("Enter Current Year");
+document.write("Current Year: ",currentYear,"<br>");
 
-// var age = currentYear - birthYear;
+var birthYear = +prompt("Enter Birth Year");
+document.write("Birth Year: ",birthYear,"<br>");
 
-// document.write("Your Age is: " , age);
+var age = currentYear - birthYear;
 
-// QUESTION 12
+document.write("Your Age is: " , age);
 
-// document.write("<h1>The Geometrizer</h1><br/>");
+// QUESTION 12) ******************************************************************************************************************
 
-// var radius = +prompt("Enter any value for radius:");
-// document.write("Radius of a circle: ",radius,"<br>")
+document.write("<h1>The Geometrizer</h1><br/>");
 
-// const pie = 3.142;
-// var circumference = 2*pie*radius;
-// document.write("The Circumference is: ",circumference,"<br/>");
+var radius = +prompt("Enter any value for radius:");
+document.write("Radius of a circle: ",radius,"<br>")
 
-// var area = pie*radius*radius
-// document.write("The Area is: ",area,"<br>")
+const pie = 3.142;
+var circumference = 2*pie*radius;
+document.write("The Circumference is: ",circumference,"<br/>");
 
-// QUESTION 13
+var area = pie*radius*radius
+document.write("The Area is: ",area,"<br>")
+
+// QUESTION 13) ******************************************************************************************************************
 
 var favSnack = "Biryani";
 document.write("Favourite Snack: ",favSnack,"<br>");
