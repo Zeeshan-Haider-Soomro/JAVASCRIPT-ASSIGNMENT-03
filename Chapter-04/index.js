bc94ebc8148ce76617da0ca96f74e943ab7350d6
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-03 06:52:13 +0500

    Update index.js

--- Chapter-04/index.js
+++ Chapter-04/index.js
@@ -1,24 +1,26 @@
-// var zeeshan , $zeeshan , _zeeshan
+var zeeshan , $zeeshan , _zeeshan
 
-// // legal variables
+// legal variables
 
-// var zeeshan
-// var $zeeshan
-// var zeeshanHaider
-// var zeeshan_haider
-// var _zeeshan
-// var zeeshan123
+var zeeshan
+var $zeeshan
+var zeeshanHaider
+var zeeshan_haider
+var _zeeshan
+var zeeshan123
 
-// // ilegal variables
+// ilegal variables
 
-// var 123zeeshan
-// var #zeeshan
-// var zeeshan-zeeshan
-// var zeeshan zeeshanHaider
-// var %zeeshan
-// var alert 
+var 123zeeshan
+var #zeeshan
+var zeeshan-zeeshan
+var zeeshan zeeshanHaider
+var %zeeshan
+var alert 
 
-document.write("<h1>Rules for naming JS variables</h1><br/><br/>Variable names can only contain letters, numbers, $ and _. For example : $my_1stVariable<br/><br/>Variables must begin with a letter, $ or _. For example : $name, _name or name<br/><br/>Variable names are case sensitive<br/><br/>Variable names should not be JS<br/><br/>");
+document.write("<h1>Rules for naming JS variables</h1><br/><br/>Variable names can only contain letters, numbers, $ and _. 
+For example : $my_1stVariable<br/><br/>Variables must begin with a letter, $ or _. For example : $name, _name or name<br/><br/>Variable names are case sensitive
+<br/><br/>Variable names should not be JS<br/><br/>");
 
 
 
